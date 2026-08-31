e474cba451eb35e4fb9f4785fdf9de6a4a70bc31
Craig Tiller <craig.tiller@gmail.com>
2026-08-31 00:34:55 -0700

    proof(memory-model): preserve envelope occurrence paths

--- Gasm.lean
+++ Gasm.lean
@@ -48,6 +48,8 @@ import Gasm.MemoryModel.CpuGraphCoherencePath
 import Gasm.MemoryModel.CpuGraphCoherencePathControls
 import Gasm.MemoryModel.Envelope
 import Gasm.MemoryModel.EnvelopeControls
+import Gasm.MemoryModel.EnvelopeOccurrencePath
+import Gasm.MemoryModel.EnvelopeOccurrencePathControls
 import Gasm.MemoryModel.BindingHistory
 import Gasm.MemoryModel.BindingHistoryControls
 import Gasm.MemoryModel.ProgramOrderPath
